6592a0650713dedf12b91be1264cf28d80246946
CreativeMeltCa <katalyst133@gmail.com>
2024-06-14 14:52:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:3001/api/products')  // Use your proxy server URL
+    fetch('https://meltstore-kjq6nv3uv-melts-projects.vercel.app/api/products')  // Use your deployed Vercel URL
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
